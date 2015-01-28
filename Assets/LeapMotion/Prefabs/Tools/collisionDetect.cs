89cbbd02560eaa20fff1351687b1b5d4f29762bf
OmarMoharrem <omarmoharrem@hotmail.com>
2015-01-28 09:03:07 -0800

    No glitch when touching the tumor with tool objects tool objects can be switchs without having to re-insert arms

--- Assets/LeapMotion/Prefabs/Tools/collisionDetect.cs
+++ Assets/LeapMotion/Prefabs/Tools/collisionDetect.cs
@@ -2,23 +2,45 @@
 using System.Collections;
 
 public class collisionDetect : MonoBehaviour {
+	public Transform target;
+	public Transform errorPoint;
+	public Transform errorPoint2;
+	public float dist;
+	public float errorDist1;
+	public float errorDist2;
 
+	public bool mistake;
 	// Use this for initialization
 	void Start () {
-	
+		target = GameObject.Find ("tumor").transform;
+		errorPoint=GameObject.Find ("default_MeshPart4").transform;
+		errorPoint2 = GameObject.Find ("default_MeshPart5").transform;
 	}
+	
 	
 	// Update is called once per frame
 	void Update () {
-	
+		dist = Vector3.Distance (transform.position, target.transform.position);
+		errorDist1 = Vector3.Distance (transform.position, errorPoint.transform.position);
+		errorDist2 = Vector3.Distance (transform.position, errorPoint2.transform.position);
+		if (dist <= 1.0f) {
+			Debug.Log("You hit the tumor, good job!");
+		}
+
+
+	}
+
+	void success()
+	{
+		Debug.Log("Success");
 	}
 	void OnCollisionEnter (Collision col)
 	{
+
+	 dist = Vector3.Distance(transform.position, target.transform.position);
+
 		if (col.gameObject.name == "default_MeshPart4" || col.gameObject.name == "default_MeshPart5") {
 						Debug.Log ("you hit the brain, dumb ass keep trying");
-		} else if (col.gameObject.name == "Sphere") {
-			Debug.Log ("good job! you hit the tumor");
-
-				}
+		} 
 	}
 }
